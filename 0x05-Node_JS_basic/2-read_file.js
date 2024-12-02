a1e9490b530efc50101305e0b341e82cf96163e3
tech-gecko <w0nderzz2001@gmail.com>
2024-12-02 20:39:23 +0100

    Update task 2 for specific checks, not generic

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,28 +3,28 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-  try {
-    const data = fs.readFileSync(filePath, 'utf-8');
-    const lines = data.split('\n');
-    console.log(`Number of students: ${lines.length - 1}`);
-
-    const csLines = lines.filter((line) => line.includes('CS'));
-    const sweLines = lines.filter((line) => line.includes('SWE'));
-
-    const csArray = [];
-    const sweArray = [];
-    for (const line of csLines) {
-      csArray.push(line.split(',')[0]);
-    }
-    for (const line of sweLines) {
-      sweArray.push(line.split(',')[0]);
-    }
-
-    console.log(`Number of students in CS: ${csLines.length}. List: ${csArray.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweLines.length}. List: ${sweArray.join(', ')}`);
-  } catch (err) {
+  if (!fs.existsSync(filePath)) {
     throw new Error('Cannot load the database');
   }
+
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const lines = data.split('\n');
+  console.log(`Number of students: ${lines.length - 1}`);
+
+  const csLines = lines.filter((line) => line.includes('CS'));
+  const sweLines = lines.filter((line) => line.includes('SWE'));
+
+  const csArray = [];
+  const sweArray = [];
+  for (const line of csLines) {
+    csArray.push(line.split(',')[0]);
+  }
+  for (const line of sweLines) {
+    sweArray.push(line.split(',')[0]);
+  }
+
+  console.log(`Number of students in CS: ${csLines.length}. List: ${csArray.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweLines.length}. List: ${sweArray.join(', ')}`);
 }
 
 module.exports = countStudents;
