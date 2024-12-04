708f82c1d8410762bf45cf3b73c6b249ce4fe14a
tech-gecko <w0nderzz2001@gmail.com>
2024-12-04 22:12:52 +0100

    Update task 4 to use Buffer.from and be exported

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,25 +2,23 @@
  * A small HTTP server, the 'Hello World' of Node.js.
  * This is for older versions. For node 16 or later, import from 'node:http'.
  */
-const { createServer } = require('http');
+const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
-const app = createServer();
 app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
 
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
   res.write(Buffer.from(responseText));
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
-
-app();
 
 module.exports = app;
