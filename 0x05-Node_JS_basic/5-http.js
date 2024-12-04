9ceaa91bc888058fb23618cfb3ac8183fec32b9b
tech-gecko <w0nderzz2001@gmail.com>
2024-12-04 22:39:08 +0100

    Update task 5 to use Buffer.from and be exported

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,41 +2,42 @@
  * A little more complex HTTP server that contains 2 different routes.
  * This is for older versions. For node 16 or later, import from 'node:http'.
  */
-const { createServer } = require('http');
-const countStudents = require('./3-read_file_async');
+const http = require('http');
 
-const app = () => {
-  const hostname = '127.0.0.1';
-  const port = 1245;
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
-  console.log('Starting server setup...');
-  const server = createServer((req, res) => {
-    if (req.url === '/') {
-      // Logic for the root route.
-      console.log("Received a request for '/'...");
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-      // Logic for the /students route.
-      console.log("Received a request for '/students'...");
-      res.statusCode = 200;
-      // Async handling.
-      countStudents(process.argv[2])
-        .then((result) => {
-          res.setHeader('Content-Type', 'text/plain');
-          res.end(`This is the list of our students\n${result}\n`);
-        })
-        .catch((err) => {
-          res.statusCode = 500; // Internal server error
-          res.end(err.message);
-        });
-    }
-  });
+app.on('request', (req, res) => {
+  if (req.url === '/') {
+    // Logic for the root route.
+    const responseText = 'Hello Holberton School!';
 
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
-};
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', responseText.length);
+    res.statusCode = 200;
+    res.write(Buffer.from(responseText));
+  } else if (req.url === '/students') {
+    // Logic for the /students route.
+    res.statusCode = 200;
+    // Async handling.
+    countStudents(process.argv[2])
+      .then((result) => {
+        const responseText = `This is the list of our students\n${result}`;
 
-app();
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Length', responseText.length);
+        res.write(Buffer.from(responseText));
+      })
+      .catch((err) => {
+        res.statusCode = 500; // Internal server error
+        res.end(err.message);
+      });
+  }
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
