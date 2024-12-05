e06385d1082a55b0928377dd3cb1d231d7a0d73d
tech-gecko <w0nderzz2001@gmail.com>
2024-12-05 12:10:01 +0100

    Add task 6, optimize for lint

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,6 +2,7 @@
  * A small HTTP server using Express module.
  */
 const express = require('express');
+
 const app = express();
 
 const PORT = 1245;
@@ -12,7 +13,7 @@ app.get('/', (_, res) => {
 });
 
 app.listen(PORT, HOST, () => {
-    process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
-})
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 module.exports = app;
