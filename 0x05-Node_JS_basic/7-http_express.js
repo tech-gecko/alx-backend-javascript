c81b0956780fd8ee58ca29474fa708af08a4b648
tech-gecko <w0nderzz2001@gmail.com>
2024-12-05 19:47:01 +0100

    Add task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,38 @@
+/**
+ * A small HTTP server using Express module.
+ */
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+const PORT = 1245;
+
+app.get('/', (_, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (_, res) => {
+  // Logic for the /students route.
+  res.set('Content-Type', 'text/plain');
+  // Better way to handle response streams than the way in the file 5*.
+  const responseParts = ['This is the list of our students\n'];
+  // Async handling.
+  countStudents(process.argv[2])
+    .then((result) => {
+      responseParts.push(result); // Add the result to the response.
+      res.status(200).send(responseParts.join('')); // Send the final response, but not as an array.
+    })
+    .catch((err) => {
+      // Send the error message with status 500.
+      // If you'd used sendStatus, it would have not sent any message after.
+      res.status(500).send('This is the list of our students\n' + err.message);
+    });
+});
+
+app.listen(PORT, () => {
+  process.stdout.write(`Server listening on port ${PORT}\n`);
+});
+
+module.exports = app;
